20df0fd14190ef3e01f02293104a3d4598469b7d
Andre Wijaya <32582821+andreewijayaa@users.noreply.github.com>
2017-11-10 15:18:52 -0700

    Update AssessorObservable.java

--- src/eggheadeducator/AssessorObservable.java
+++ src/eggheadeducator/AssessorObservable.java
@@ -5,16 +5,36 @@ import java.util.Observable;
 public class AssessorObservable extends Observable
 {
 	private boolean correct;
+	private int wrongAttempt = 0;
 	
 	public void correctness(boolean correct)
 	{
 		this.correct = correct;
 		setChanged();
 		notifyObservers();
+		
+		if(correct)
+		{
+			wrongAttempt = 0;
+		}
+		else
+		{
+			wrongAttempt++;
+		}
 	}
 	
 	public boolean getCorrectness()
 	{
 		return correct;
+	}
+	
+	public int getWrongAttempt()
+	{
+		return wrongAttempt;
+	}
+	
+	public void resetAttempt()
+	{
+		wrongAttempt = 0;
 	}
 }
